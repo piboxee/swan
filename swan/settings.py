806f70361681337bdb620e2843ece8459051a624
Valentin Piho <pibox.ee@gmail.com>
2020-04-09 21:34:43 +0300

    Share blog post with Email

--- swan/settings.py
+++ swan/settings.py
@@ -53,6 +53,8 @@ MIDDLEWARE = [
 
 ROOT_URLCONF = 'swan.urls'
 
+EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
+
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
