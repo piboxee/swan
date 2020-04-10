c6c2d06a5ea30c5cc180f3cc336d3760be388a7d
Valentin Piho <pibox.ee@gmail.com>
2020-04-10 09:42:24 +0300

    RSS Feed added

--- blog/urls.py
+++ blog/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 
+from .feeds import LatestPostsFeed
 from . import views
 
 app_name = 'blog'
@@ -12,4 +13,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>', views.post_detail, name='post_detail'),
     path('<int:post_id>/share/', views.post_share, name='post_share'),
 
+    path('feed/', LatestPostsFeed(), name='post_feed'),
 ]
