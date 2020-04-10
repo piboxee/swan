df6bb87aa6d5defe85124703c31d92d908fa69fe
Valentin Piho <pibox.ee@gmail.com>
2020-04-10 20:02:38 +0300

    User registration. Profile edit. Site system messages

--- swan/urls.py
+++ swan/urls.py
@@ -1,6 +1,8 @@
 from django.contrib import admin
 from django.urls import path, include
 from django.contrib.sitemaps.views import sitemap
+from django.conf import settings
+from django.conf.urls.static import static
 
 from blog.sitemaps import PostSitemap
 
@@ -14,3 +16,7 @@ urlpatterns = [
     path('blog/', include('blog.urls', namespace='blog')),
     path('sitemap.xml', sitemap, {'sitemaps': sitemaps}, name='django.contrib.sitemaps.views.sitemap')
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+
