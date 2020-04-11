1fbb26cb410940c708c497d85f13ab9eb2d1d8ea
Valentin Piho <pibox.ee@gmail.com>
2020-04-11 11:02:41 +0300

    Custom authentication with Email

--- swan/settings.py
+++ swan/settings.py
@@ -93,6 +93,11 @@ DATABASES = {
 }
 
 
+AUTHENTICATION_BACKENDS = [
+    'django.contrib.auth.backends.ModelBackend',
+    'account.authentication.EmailAuthBackend'
+]
+
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
 
